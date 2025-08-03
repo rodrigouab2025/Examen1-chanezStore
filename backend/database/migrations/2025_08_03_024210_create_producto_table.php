aeaedf47ab05bcc47f6d9c3f923e4c2786f2ed75
rodrigouab2025 <rodrigo.chanez@uab.edu.bo>
2025-08-02 22:49:03 -0400

    crear migracion producto

---
+++ backend/database/migrations/2025_08_03_024210_create_producto_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('producto', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',50);
+            $table->decimal('precio', 10, 2);
+            $table->unsignedBigInteger('marca_id');
+            $table->timestamps();
+            $table->foreign('marca_id')->references('id')->on('marcas')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('producto');
+    }
+};
